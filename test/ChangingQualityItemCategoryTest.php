e2d6835fb63156875d95982c283fd12ae275b0e4
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 20:55:17 +0300

    Added test that checks if changing quality item category doesn't make item's quality less than the minimum allowed value

--- test/ChangingQualityItemCategoryTest.php
+++ test/ChangingQualityItemCategoryTest.php
@@ -12,4 +12,11 @@ class ChangingQualityItemCategoryTest extends \PHPUnit\Framework\TestCase {
         $category->updateItem($item);
         $this->assertEquals(-1, $item->sell_in);
     }
+
+    public function testUpdateItemShouldNotMakeItemQualityLessThanMinQuality() {
+        $category = new ChangingQualityItemCategory('changing', [new QualityUpdateRate(-5, 0)], 0, 50);
+        $item = new Item('item', 1, 3);
+        $category->updateItem($item);
+        $this->assertEquals(0, $item->quality);
+    }
 }
