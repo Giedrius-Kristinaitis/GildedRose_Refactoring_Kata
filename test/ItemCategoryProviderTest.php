b91daed4941dda308b14f5aec7ccb75c882071ff
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 21:31:54 +0300

    Added test that checks if item category provider provides exceptional item category for exceptional item

--- test/ItemCategoryProviderTest.php
+++ test/ItemCategoryProviderTest.php
@@ -13,4 +13,14 @@ class ItemCategoryProviderTest extends \PHPUnit\Framework\TestCase {
 
         $this->assertEquals($normal_category, $category_provider->getItemCategory('non-exceptional item'));
     }
+
+    public function testGetCategoryShouldReturnExceptionalCategoryBecauseItemIsExceptional() {
+        $normal_category = new ChangingQualityItemCategory('normal', [new QualityUpdateRate(1, 0)], 0, 50);
+        $exceptional_item_categories = [new FixedQualityItemCategory('fixed', 0)];
+        $exceptional_items = ['exceptional_item' => 'fixed'];
+
+        $category_provider = new ItemCategoryProvider($normal_category, $exceptional_item_categories, $exceptional_items);
+
+        $this->assertEquals($exceptional_item_categories[0], $category_provider->getItemCategory('exceptional_item'));
+    }
 }
