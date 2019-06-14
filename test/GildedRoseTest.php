8fefaa72f001429a20e81e1cc94890b3d334d0db
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 22:04:47 +0300

    Created test that checks if legendary item's quality stays the same

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -4,5 +4,15 @@ namespace App;
 
 class GildedRoseTest extends \PHPUnit\Framework\TestCase {
     
-    
+    public function testLegendaryItemShouldNotChangeQualityBeforeSellDate() {
+        $category_provider = new class() implements ItemCategoryProviderInterface {
+            public function getItemCategory($item_name) {
+                return new FixedQualityItemCategory('fixed', 80);
+            }
+        };
+        
+        $items = [new Item('legendary item', 10, 80)];
+        $gilded_rose = new GildedRose($items, $category_provider);
+        $this->assertEquals(80, $items[0]->quality);
+    }
 }
