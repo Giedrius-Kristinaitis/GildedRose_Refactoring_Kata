cdefeee63d0748f4f65a55520946fd808467c980
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 20:22:45 +0300

    Added test that checks if fixed quality item category doesn't change item's quality

--- test/FixedQualityItemCategoryTest.php
+++ test/FixedQualityItemCategoryTest.php
@@ -15,4 +15,14 @@ class FixedQualityItemCategoryTest extends \PHPUnit\Framework\TestCase {
 
         $this->assertEquals(0, $item->sell_in);
     }
+
+    public function testUpdateItemShouldNotChangeItemQuality() {
+        $category = new FixedQualityItemCategory('fixed', 80);
+
+        $item = new Item('Sulfuras', 0, 80);
+
+        $category->updateItem($item);
+
+        $this->assertEquals(80, $item->quality);
+    }
 }
