64a14de3f34ac4421d5b740f9e69a400160a16c4
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 21:33:35 +0300

    Slightly modified item category provider test

--- test/ItemCategoryProviderTest.php
+++ test/ItemCategoryProviderTest.php
@@ -16,11 +16,11 @@ class ItemCategoryProviderTest extends \PHPUnit\Framework\TestCase {
 
     public function testGetCategoryShouldReturnExceptionalCategoryBecauseItemIsExceptional() {
         $normal_category = new ChangingQualityItemCategory('normal', [new QualityUpdateRate(1, 0)], 0, 50);
-        $exceptional_item_categories = [new FixedQualityItemCategory('fixed', 0)];
-        $exceptional_items = ['exceptional_item' => 'fixed'];
+        $exceptional_item_categories = [new FixedQualityItemCategory('fixed 1', 10), new FixedQualityItemCategory('fixed 2', 0)];
+        $exceptional_items = ['exceptional_item' => 'fixed 2'];
 
         $category_provider = new ItemCategoryProvider($normal_category, $exceptional_item_categories, $exceptional_items);
 
-        $this->assertEquals($exceptional_item_categories[0], $category_provider->getItemCategory('exceptional_item'));
+        $this->assertEquals($exceptional_item_categories[1], $category_provider->getItemCategory('exceptional_item'));
     }
 }
