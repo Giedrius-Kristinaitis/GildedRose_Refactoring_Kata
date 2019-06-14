881ce83966ce49ec2d4f3042236ba69c9d882e22
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 22:07:02 +0300

    Added test to make sure legendary item's quality stays the same if sell_in is less than 0

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -10,8 +10,20 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase {
                 return new FixedQualityItemCategory('fixed', 80);
             }
         };
-        
+
         $items = [new Item('legendary item', 10, 80)];
+        $gilded_rose = new GildedRose($items, $category_provider);
+        $this->assertEquals(80, $items[0]->quality);
+    }
+
+    public function testLegendaryItemShouldNotChangeQualityAfterSellDate() {
+        $category_provider = new class() implements ItemCategoryProviderInterface {
+            public function getItemCategory($item_name) {
+                return new FixedQualityItemCategory('fixed', 80);
+            }
+        };
+        
+        $items = [new Item('legendary item', -1, 80)];
         $gilded_rose = new GildedRose($items, $category_provider);
         $this->assertEquals(80, $items[0]->quality);
     }
