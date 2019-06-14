c5692d91b7011bf3da08f677f96fca9bcf6f3930
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-06-14 21:21:11 +0300

    Added test that checks if fixed quality after sell in item category does change item's quality to fixed after sell date

--- test/FixedQualityAfterSellInItemCategoryTest.php
+++ test/FixedQualityAfterSellInItemCategoryTest.php
@@ -10,4 +10,11 @@ class FixedQualityAfterSellInItemCategoryTest extends \PHPUnit\Framework\TestCas
         $category->updateItem($item);
         $this->assertNotEquals(10, $item->quality);
     }
+
+    public function testUpdateItemShouldSetItemQualityToFixedAfterSellDate() {
+        $category = new FixedQualityAfterSellInItemCategory('fixed after', 10, [new QualityUpdateRate(-1, 0)], 0, 50);
+        $item = new Item('item', -1, 20);
+        $category->updateItem($item);
+        $this->assertEquals(10, $item->quality);
+    }
 }
